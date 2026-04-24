5dbb66a536f6012d5aedc40e6d4bec118518b8aa
Sourish Dey <sourish19z@gmail.com>
2026-04-25 00:46:25 +0530

    add ass4 & ass13

---
+++ lab_assignments/ass13.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+class Box {
+    int l;
+    int b;
+    int h;
+
+    Box(int l, int b, int h) {
+        this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+
+    int volume() {
+        int vol = this.l * this.b * this.h;
+        return vol;
+    }
+
+}
+
+class BoxWeight extends Box {
+    int w;
+
+    BoxWeight(int l, int b, int h, int w) {
+        super(l, b, h);
+        this.w = w;
+    }
+}
+
+class Shipment extends BoxWeight {
+    int costPerUnitVol;
+    int costPerUnitWeight;
+
+    Shipment(int l, int b, int h, int w, int costPerUnitVol, int costPerUnitWeight) {
+        super(l, b, h, w);
+        this.costPerUnitVol = costPerUnitVol;
+        this.costPerUnitWeight = costPerUnitWeight;
+    }
+
+    int calculateShippingCost() {
+        return ((this.costPerUnitVol * this.volume()) + (this.costPerUnitWeight * this.w));
+    }
+}
+
+public class ass13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length: ");
+        int l = sc.nextInt();
+
+        System.out.print("Enter breadth: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter height: ");
+        int h = sc.nextInt();
+
+        System.out.print("Enter weight: ");
+        int w = sc.nextInt();
+
+        System.out.print("Enter cost per unit volume: ");
+        int costPerUnitVol = sc.nextInt();
+
+        System.out.print("Enter cost per unit weight: ");
+        int costPerUnitWeight = sc.nextInt();
+
+        Shipment s = new Shipment(l, b, h, w, costPerUnitVol, costPerUnitWeight);
+        int vol = s.volume();
+        System.out.println("Volume of Rectangular Parcel: " + vol);
+        int cost = s.calculateShippingCost();
+        System.out.println("Shipping cost of the parcel: " + cost);
+    }
+}
---
+++ lab_assignments/ass4.java
@@ -0,0 +1,17 @@
+class Cone {
+    Cone(int r, int h) {
+        System.out.println("Volume of a cone with arguments: " + ((1.0 / 3) * Math.PI * (r * r) * h));
+    }
+
+    Cone() {
+        int r = 3, h = 5;
+        System.out.println("Volume of a cone with no arguments: " + ((1.0 / 3) * Math.PI * (r * r) * h));
+    }
+}
+
+public class ass4 {
+    public static void main(String[] args) {
+        Cone c1 = new Cone(3, 5);
+        Cone c2 = new Cone();
+    }
+}
