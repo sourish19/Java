16e1d44d255eb3c2eb062827e4524b9916b7de83
Sourish Dey <sourish19z@gmail.com>
2026-04-29 09:29:21 +0530

    add ass7

--- lab_assignments/ass7.java
+++ lab_assignments/ass7.java
@@ -1,13 +1,37 @@
-class Stack{
+import java.util.Scanner;
+
+class Stack {
     int[] arr = new int[5];
+    int top = 0;
 
-    void push(){}
+    void push(int val) {
+        this.arr[top] = val;
+        this.top++;
+    }
 
-    void pop(){}
-    
-    void display(){}
+    void pop() {
+        this.top--;
+    }
+
+    void display() {
+        System.out.print("Stack: ");
+        for (int i = 0; i < top; i++) {
+            System.out.print(this.arr[i] + " ");
+        }
+    }
 }
 
 public class ass7 {
-    
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        Scanner sc = new Scanner(System.in);
+
+        stack.push(sc.nextInt());
+        stack.push(sc.nextInt());
+        stack.push(sc.nextInt());
+
+        stack.pop();
+
+        stack.display();
+    }
 }
